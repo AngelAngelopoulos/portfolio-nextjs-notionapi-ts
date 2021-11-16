1714a9fffea583694bd9d47456a625aa00d8c9aa
Angel Alvarado <angel.alvarado.dev@gmail.com>
2021-11-15 20:37:39 -0600

    first commit

--- components/PageComponents/ImageProfile.tsx
+++ components/PageComponents/ImageProfile.tsx
@@ -3,9 +3,7 @@ import { NextPage } from 'next';
 export const ImageProfile: NextPage = () => {
   return (
     <img className='w-full'
-      //src="https://f002.backblazeb2.com/file/laravel-portfolio/assets/img/misc/Angelo-min.jpg"
       src="image.png"
-
     />
   );
 };
